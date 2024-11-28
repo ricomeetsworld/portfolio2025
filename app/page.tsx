258172162fd86f8d5fc861a52a8d186da8f06ec9
Enric Trenric_trillo <enric_trillo@outlook.com>
2024-11-28 13:57:46 +0000

    update: footer section, styled newsletter section

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
           <span className="relative flex shrink-0 overflow-hidden rounded-full w-24 h-24"><img className="aspect-square h-full w-full" alt="Profile Picture" src="https://v0.dev/placeholder.svg" /></span>
           <div className="space-y-2">
             <h1 className="text-2xl font-bold">Enric Trillo</h1>
-            <p className="text-muted-foreground">Building with disruptive tech</p>
+            <p className="font-medium text-muted-foreground">Building with disruptive tech</p>
             <p className="text-muted-foreground">My name is Enric Trillo and I'm a fullstack developer, wannabepreneur and creator based in 🇬🇧 London. Focused on disruptive technologies. Nice to have you here! 👋</p>
           </div>
         </div>
-        <div id="newsletter" className="space-y-4">
-          <h2 className="heading2">Email Newsletter</h2>
+        <div id="newsletter" className="space-y-4 bg-slate-100 p-6 rounded-md">
+          <h2 className="text-xl font-semibold">Email Newsletter</h2>
           <p>Join <strong>[Shift Forward]</strong> for disruptive tech updates, exclusive offers & members only content directly from me. We don't do spam over here, I promise!</p>
           <form className="flex flex-col gap-4">
             <div className="">
@@ -41,6 +41,11 @@ export default function Home() {
             >Become a Shifter</button>
           </form>
         </div>
+        <footer className="mt-16 border-t border-white/15 pt-8 sm:flex items-center flex-row sm:justify-between">
+          <p className="text-sm leading-5 text-muted-foreground">
+            <p>© {new Date().getFullYear()} Enric Trillo. All rights reserved.</p>
+          </p>
+        </footer>
       </div>
     </main>
   );
